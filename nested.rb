79cf1f52717285a35b2da88b902b2065f73210fc
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 01:23:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,14 @@ def adding_matz
         }
      }
 
-  programmer_hash.push()  
+  programmer_hash.merge!
+  (
+    { :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+      }
+    }  
+  )  
 end
 
 def changing_alan
